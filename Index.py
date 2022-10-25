f9a18db788a01d111a396ee5d9d3a9201c1d8e4c
JOÃO SANTOS <116624654+Gingseng@users.noreply.github.com>
2022-10-25 13:55:42 -0400

    Create Index.py

---
+++ Index.py
@@ -0,0 +1,10 @@
+com = float (input ('digite o capital:'))
+com2 = float (input ('digite a porcentagem de juros aplicados (variavel em float):'))
+com3 = float (input ('digite o tempo determinado do : '))
+resultado = (com*com2*com3)
+
+print ("o juro recorrente é de")
+print (resultado)
+
+print ('portanto o montante acumulado é de:')
+print (com+resultado)
